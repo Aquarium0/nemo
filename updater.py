4d199e7d9c96b0ef9eafa45a9164b2dfb3d04089
Dominic <Dominic.Cheung@qbt.travel>
2023-04-05 15:12:03 +1000

    Reducing unnecessary constant

--- updater.py
+++ updater.py
@@ -5,15 +5,15 @@ import sys
 REPO_LINK = 'https://github.com/Aquarium0/nemo/blob/main/{}?raw=true'
 VERSION_LINK = 'https://raw.githubusercontent.com/Aquarium0/nemo/main/version.json'
 
-VALID_FILES = ['AGM1', ]
 # name-vx.x
 
 def checkUpdate():
     versions = requests.get(VERSION_LINK).json()
+    validFiles = [name for name in versions]
 
     for fileName in os.listdir(os.path.dirname(sys.executable)):
         appName = fileName.split('-')[0]
-        if appName in VALID_FILES and 'exe' in fileName:
+        if appName in validFiles and 'exe' in fileName:
             versionData = fileName.split('-')[1]
             if versionData[:-4] != versions[appName]:
                 pullUpdate(f"{appName}-{versions[appName]}.exe")
@@ -27,10 +27,11 @@ def pullUpdate(fileName):
 
 def checkPurge():
     versions = requests.get(VERSION_LINK).json()
+    validFiles = [name for name in versions]
 
     for fileName in os.listdir(os.path.dirname(sys.executable)):
         appName = fileName.split('-')[0]
-        if appName in VALID_FILES and 'exe' in fileName:
+        if appName in validFiles and 'exe' in fileName:
             versionData = fileName.split('-')[1]
             if versionData[:-4] != versions[appName]:
                 os.remove(fileName)
