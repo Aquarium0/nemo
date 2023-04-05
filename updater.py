b77ae8c00f926908be69b6a09a0f0c1e3993f2fd
Dominic <Dominic.Cheung@qbt.travel>
2023-04-06 09:04:45 +1000

    Modified the updater for the old running application, rather than the first instance of an old application

--- updater.py
+++ updater.py
@@ -1,23 +1,27 @@
 import os
 import requests
 import sys
-
+from time import sleep
 REPO_LINK = 'https://github.com/Aquarium0/nemo/blob/main/{}?raw=true'
 VERSION_LINK = 'https://raw.githubusercontent.com/Aquarium0/nemo/main/version.json'
 
 # name-vx.x
 
 def checkUpdate():
+    print("Checking for updates...")
     versions = requests.get(VERSION_LINK).json()
+    curFile = sys.argv[0].split('\\')[-1].replace('.exe','').split('-')
     validFiles = [name for name in versions]
 
     for fileName in os.listdir(os.path.dirname(sys.executable)):
         appName = fileName.split('-')[0]
-        if appName in validFiles and 'exe' in fileName:
+        if appName in validFiles and appName == curFile[0] and 'exe' in fileName:
             versionData = fileName.split('-')[1]
             if versionData[:-4] != versions[appName]:
+                print(f"Found an update for {appName}. The program will download the update and automatically restart, please wait...")
                 pullUpdate(f"{appName}-{versions[appName]}.exe")
-                os.system("{appName}-{versions[appName]}.exe")
+                os.system(f"start {appName}-{versions[appName]}.exe")
+                sleep(0.2)
                 return True
 
 def pullUpdate(fileName):
@@ -25,7 +29,6 @@ def pullUpdate(fileName):
     with open(fileName, 'wb') as f:
         f.write(fileData.content)
     
-
 def checkPurge():
     versions = requests.get(VERSION_LINK).json()
     validFiles = [name for name in versions]
@@ -40,5 +43,4 @@ def checkPurge():
             versionData = fileName.split('-')[1]
             if versionData[:-4] != versions[appName]:
                 os.remove(fileName)
-                os.system(f'{appName}-{versions[appName]}.exe')
                 return True
